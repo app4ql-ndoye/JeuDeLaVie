bd8e01f5cb379805b79fc2f27c4f0612e5863d5c
Christelle Ngo Bui Hung <cngobui@a-109620.ups.u-psud.fr>
2016-05-09 15:28:36 +0200

    index.js : ajout de parametres

--- index.js
+++ index.js
@@ -2,21 +2,24 @@ window.onload = function() {
   var canvas = document.getElementById('can');
   var context = canvas.getContext('2d');
   var a = [];
-  setInterval(anime, 1000);
+  var taille = 10;
+  var nb= 1000;
+
+  setInterval(anime, nb);
  
   function anime() {
       context.fillStyle = "green";
       context.clearRect(0, 0, canvas.width, canvas.height);
-      for (i=0; i<10; i++) {
-          for (j=0; j<10; j++) {
+      for (i=0; i<taille; i++) {
+          for (j=0; j<taille; j++) {
               if (Math.random() > 0.9) {
-                  a[i*10+j] = 1;
+                  a[i*taille+j] = 1;
                   context.fillStyle = "green";
               } else {
-                  a[i*10+j] = 0;
+                  a[i*taille+j] = 0;
                   context.fillStyle = "red";
               }
-              context.fillRect(10*i, 10*j, 10, 10);
+              context.fillRect(taille*i, taille*j, taille, taille);
           }
       }
   }
